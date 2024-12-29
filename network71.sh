0f2e4d7107c8a92d72c90599d2310af48f9ebb4d
Blake Garner <blake@netjibbing.com>
2024-12-28 19:22:45 -0800

    begin

---
+++ network71.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+shopt -s nullglob nocaseglob
+
+# modern bash version check
+! [ "${BASH_VERSINFO:-0}" -ge 4 ] && echo "This script requires bash v4 or later" && exit 1
+
+# path to self and parent dir
+SCRIPT=$(realpath $0)
+SCRIPTPATH=$(dirname $SCRIPT)
+
+# check for qemu-system-m68k
+if [ ! -f "/opt/local/bin/qemu-system-m68k" ]; then
+  echo "qemu-system-m68k not found"
+  exit 1
+fi
+
+# check for Quadra_800.rom
+if [ ! -f "$SCRIPTPATH/Quadra_800.rom" ]; then
+  echo "Quadra_800.rom not found"
+  exit 1
+fi
+
+# create pram-q800.img if it doesn't exist
+if [ ! -f "$SCRIPTPATH/pram_q800_712.img" ]; then
+  echo "Creating pram_q800_712.img"
+  /opt/local/bin/qemu-img create -f raw pram_q800_712.img 256b
+fi
+
+# check for Macintosh-Quadra-650-v71.cdr
+if [ ! -f "$SCRIPTPATH/Macintosh-Quadra-650-v71.cdr" ]; then
+  echo "Macintosh-Quadra-650-v71.cdr not found"
+  exit 1
+fi
+
+# check for quadra800_712.qcow2
+if [ ! -f "$SCRIPTPATH/quadra800_712.qcow2" ]; then
+  echo "quadra800_712.qcow2 not found"
+  /opt/local/bin/qemu-img create -f qcow2 quadra800_712.qcow2 2G
+fi
+
+# install boot
+sudo /opt/local/bin/qemu-system-m68k \
+  -M q800 \
+  -m 64 \
+  -bios "$SCRIPTPATH/Quadra_800.rom" \
+  -g 1152x870x8 \
+  -drive file=pram_q800_712.img,format=raw,if=mtd \
+  -device scsi-hd,scsi-id=0,drive=hd0 \
+  -drive "file=$SCRIPTPATH/quadra800_712.qcow2,media=disk,format=qcow2,if=none,id=hd0" \
+  -device scsi-cd,scsi-id=3,drive=cd0 \
+  -drive "file=$SCRIPTPATH/SuperDuo.iso,media=cdrom,if=none,id=cd0" \
+  -nic vmnet-bridged,model=dp83932,mac=08:00:07:12:34:56,ifname=en0 \
+  -audio driver=none
+                                                                                                                                                                                                                                                                                                                                                                                  #   -nic vmnet-bridged,model=dp83932,mac=08:00:07:A2:A2:A2,ifname=en0
+
+exit 0
